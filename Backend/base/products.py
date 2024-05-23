0fb920de1da9ac4f0d3685ff865c890c6eeead41
sirajamsamira17 <84776328+sirajamsamira17@users.noreply.github.com>
2024-05-23 00:02:44 -0700

    Add files via upload

---
+++ Backend/base/products.py
@@ -0,0 +1,81 @@
+products = [
+    {
+      '_id': '1',
+      'name': 'Cheesy Burger',
+      'image': '/images/burger.jpg',
+      'description':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      
+      'category': 'Electronics',
+      'price': 89.99,
+       'countINStock':10,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'BBQ Pizza',
+      'image': '/images/pizza1.jpg',
+      'description':
+        'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+   
+      'category': 'Electronics',
+      'price': 599.99,
+      'countINStock':7,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Grilled Sandwitch',
+      'image': '/images/grilledSandwitch.jpg',
+      'description':
+        'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    
+      'category': 'Electronics',
+      'price': 929.99,
+       'countINStock':6,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'French Fry',
+      'image': '/images/frenchfry.jpg',
+      'description':
+        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      
+      'category': 'Electronics',
+      'price': 399.99,
+       'countINStock':5,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'Chicken Momos',
+      'image': '/images/momo.jpg',
+      'description':
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+
+      'category': 'Electronics',
+      'price': 49.99,
+       'countINStock':20,
+      'rating': 3.5,
+      'numReviews': 10,
+    },
+    {
+      '_id': '6',
+      'name': 'Mac and Cheese',
+      'image': '/images/macandcheese.jpg',
+      'description':
+        'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      
+      'category': 'Electronics',
+      'price': 29.99,
+     'countINStock':12,
+      'rating': 4,
+      'numReviews': 12,
+    }
+  ]
+  
